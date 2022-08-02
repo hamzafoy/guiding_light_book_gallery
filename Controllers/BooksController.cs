2bf529016d55f8d07042a9118e7d71a206d2b340
hamzafoy <hamzaoflouisville@gmail.com>
2022-08-02 16:52:41 -0400

    Add project files.

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GuidingLightBookGallery.Controllers
+{
+    public class BooksController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public ActionResult Details()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GuidingLightBookGallery.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
