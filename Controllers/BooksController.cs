b856f4bc6c21873939936ce8985601db3836d841
hamzafoy <hamzaoflouisville@gmail.com>
2022-08-03 17:19:20 -0400

    Brought data to the BooksController.cs

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -11,6 +11,15 @@ namespace GuidingLightBookGallery.Controllers
         
         public ActionResult Details()
         {
+            ViewBag.Title = "Islam and the World";
+            ViewBag.Authors = new string[]
+            {
+                "Author: Sayyed Abul Hasan Ali Nadwi",
+                "Translator: Dr. Muhammad Kidwai"
+            };
+            ViewBag.Description = "<p>This book persuasively argues against the premise that Islam is a <em>spent force</em>, reminding readers of its dynamism.</p>";
+            ViewBag.PublishingHouse = "UK Islamic Academy";
+            ViewBag.PublicationYear = 2005;
             return View();
         }
     }
