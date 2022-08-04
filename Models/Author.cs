c8693bb758ef78fdd431b42e73d94435904b72fd
hamzafoy <hamzaoflouisville@gmail.com>
2022-08-04 17:19:53 -0400

    Creating two models: books for the main data model & author that will be brought in as a collection for each book.

---
+++ Models/Author.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+
+namespace GuidingLightBookGallery.Models
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Role { get; set; }
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+
+namespace GuidingLightBookGallery.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int PublicationYear { get; set; }
+        public string PublishingHouse { get; set; }
+        public string DescriptionHTML { get; set; }
+        public Author[] Authors { get; set; }
+    }
+}
