6e5cc95054fce297c32e8ea8a92e58d48a9f04a9
hamzafoy <hamzaoflouisville@gmail.com>
2022-08-07 17:47:17 -0400

    Using strongly typed view instead of Viewbag.

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GuidingLightBookGallery.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GuidingLightBookGallery.Controllers
 {
@@ -8,19 +9,31 @@ namespace GuidingLightBookGallery.Controllers
         {
             return View();
         }
-        
+
         public ActionResult Details()
         {
-            ViewBag.Title = "Islam and the World";
-            ViewBag.Authors = new string[]
+            var book = new Book()
             {
-                "Author: Sayyed Abul Hasan Ali Nadwi",
-                "Translator: Dr. Muhammad Kidwai"
+                Title = "Islam and the World",
+                Authors = new Author[]
+                {
+                        new Author() { FirstName = "Sayyed Abul Hasan", LastName = "Ali Nadwi", Role = "Author" },
+                        new Author() { FirstName = "Muhammad", LastName = "Kidwai", Role = "Translator" }
+                },
+                DescriptionHTML = "<p>This book persuasively argues against the premise that Islam is a <em>spent force</em>, reminding readers of its dynamism.</p>",
+                PublishingHouse = "UK Islamic Academy",
+                PublicationYear = 2005
             };
-            ViewBag.Description = "<p>This book persuasively argues against the premise that Islam is a <em>spent force</em>, reminding readers of its dynamism.</p>";
-            ViewBag.PublishingHouse = "UK Islamic Academy";
-            ViewBag.PublicationYear = 2005;
-            return View();
+            //ViewBag.Title = "Islam and the World";
+            //ViewBag.Authors = new string[]
+            //{
+            //    "Author: Sayyed Abul Hasan Ali Nadwi",
+            //    "Translator: Dr. Muhammad Kidwai"
+            //};
+            //ViewBag.Description = 
+            //ViewBag.PublishingHouse = "UK Islamic Academy";
+            //ViewBag.PublicationYear = 2005;
+            return View(book);
         }
     }
 }
